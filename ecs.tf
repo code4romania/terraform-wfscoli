d30ae28fdd87bba6f1eba9173965c9bf4cfdece0
Andrei Ioniță <hi@andrei.io>
2025-04-22 16:56:39 +0100

    feat: ecs-cluster

---
+++ ecs.tf
@@ -0,0 +1,66 @@
+module "cluster" {
+  source  = "code4romania/ecs-cluster/aws"
+  version = "0.1.3"
+
+  namespace             = local.namespace
+  vpc_id                = module.networking.vpc_id
+  ecs_subnets           = module.networking.private_subnets
+  security_groups       = [aws_security_group.ecs.id]
+  default_instance_type = "m5a.large"
+  instance_types = {
+    "m5a.large" = ""
+    "m5.large"  = ""
+  }
+
+  min_size                  = 1
+  max_size                  = 4
+  minimum_scaling_step_size = 1
+  maximum_scaling_step_size = 1
+
+  target_capacity                          = 100
+  capacity_rebalance                       = true
+  on_demand_base_capacity                  = 0 # could be set to 1 for stability. consider savings plan
+  on_demand_percentage_above_base_capacity = 100
+  ecs_cloudwatch_log_retention             = 30
+  userdata_cloudwatch_log_retention        = 30
+
+  spot_allocation_strategy = "price-capacity-optimized"
+  spot_instance_pools      = 0
+}
+
+resource "aws_security_group" "ecs" {
+  name        = "${local.namespace}-ecs"
+  description = "Inbound Security Group attached to the ECS Service (${var.env})"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Load balancer traffic"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb.id]
+  }
+
+  ingress {
+    description = "Internal traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+  }
+
+  # ingress {
+  #   description     = "Bastion access"
+  #   from_port       = 0
+  #   to_port         = 0
+  #   protocol        = "-1"
+  #   security_groups = [aws_security_group.bastion.id]
+  # }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
