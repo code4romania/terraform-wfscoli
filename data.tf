6d6b4b6f27e9d303240f37cef5951f99ff338ea6
Andrei Ioniță <hi@andrei.io>
2025-04-30 15:47:28 +0100

    wip

---
+++ data.tf
@@ -0,0 +1,4 @@
+data "aws_availability_zones" "current" {}
+
+data "aws_region" "current" {}
+
--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
 locals {
   namespace = "${var.subdomain}-wfscoli-${var.env}"
+
+  availability_zone = data.aws_availability_zones.current.names[0]
 }
