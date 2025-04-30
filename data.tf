eacc3fb8aa55bde77e3fb2cb13a3c36ad4aeb4bc
Andrei Ioniță <hi@andrei.io>
2025-04-30 16:04:04 +0100

    wip

--- data.tf
+++ data.tf
@@ -1,4 +1,5 @@
 data "aws_availability_zones" "current" {}
 
-data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
 
+data "aws_region" "current" {}
--- database_lambda.tf
+++ database_lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "create_database" {
   function_name    = "${local.namespace}-create-database"
   handler          = "index.handler"
   runtime          = "nodejs22.x"
-  role             = aws_iam_role.lambda_role.arn
+  role             = aws_iam_role.lambda_create_database.arn
   filename         = data.archive_file.lambda_create_database.output_path
   source_code_hash = data.archive_file.lambda_create_database.output_base64sha256
   memory_size      = 128
@@ -24,8 +24,49 @@ resource "aws_lambda_function" "create_database" {
       DB_HOST    = "${aws_db_instance.main.address}"
       DB_PORT    = "${aws_db_instance.main.port}"
       DB_NAME    = "${aws_db_instance.main.username}"
-      AWS_REGION = "${var.region}"
+      AWS_REGION = "${data.aws_region.current.id}"
     }
+  }
+}
+
+resource "aws_iam_role" "lambda_create_database" {
+  name               = "${local.namespace}-lambda-create-database"
+  assume_role_policy = data.aws_iam_policy_document.lambda_create_database_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_create_database_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+
+resource "aws_iam_role_policy" "lambda_create_database_role" {
+  name   = "rds-access"
+  role   = aws_iam_role.lambda_create_database.id
+  policy = data.aws_iam_policy_document.lambda_create_database.json
+}
+
+data "aws_iam_policy_document" "lambda_create_database" {
+  statement {
+    actions = [
+      "rds-db:connect",
+    ]
+
+    resources = [
+      format(
+        "arn:aws:rds-db:%s:%s:dbuser:%s/%s",
+        data.aws_region.current.id,
+        data.aws_caller_identity.current.account_id,
+        aws_db_instance.main.resource_id,
+        aws_db_instance.main.username
+      ),
+    ]
   }
 }
 
