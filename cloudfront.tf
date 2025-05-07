02fcaec8b6064a2e686c6e3580c048f767d5f485
Andrei Ioniță <hi@andrei.io>
2025-05-07 14:57:15 +0100

    fix

--- cloudfront.tf
+++ cloudfront.tf
@@ -30,7 +30,9 @@ resource "aws_cloudfront_distribution" "media_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = aws_acm_certificate.media.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
