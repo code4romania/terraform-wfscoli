ab3e7fc20345a0e111b3957dd1554f24c3a3f890
Andrei Ioniță <hi@andrei.io>
2025-05-09 17:20:03 +0100

    wip

--- ecs.tf
+++ ecs.tf
@@ -1,6 +1,6 @@
 module "cluster" {
   source  = "code4romania/ecs-cluster/aws"
-  version = "0.1.4"
+  version = "0.1.5"
 
   namespace             = local.namespace
   vpc_id                = module.networking.vpc_id
