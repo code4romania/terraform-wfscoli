4f3e9070bf1a74847580d0e1e8d23142af043fba
Andrei Ioniță <hi@andrei.io>
2025-05-12 14:46:46 +0100

    wip

--- services.tf
+++ services.tf
@@ -1,6 +1,6 @@
 module "wfservice" {
   source  = "code4romania/ecs-service-wfscoli/aws"
-  version = "0.1.7"
+  version = "0.1.8"
 
   count    = length(local.services)
   name     = local.services[count.index].name
