1b86f4e81e2181aa9dcbfc0214186e61b6690bbc
Andrei Ioniță <hi@andrei.io>
2025-05-16 10:15:27 +0100

    wip

--- clusters/01/route53.tf
+++ clusters/01/route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_zone" "main" {
   name = "${var.subdomain}.wfscoli.ro"
 }
 
-# Cloudfront A record
+# Cloudfront
 resource "aws_route53_record" "media_ipv4" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "media"
@@ -15,7 +15,6 @@ resource "aws_route53_record" "media_ipv4" {
   }
 }
 
-# Cloudfront AAAA record
 resource "aws_route53_record" "media_ipv6" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "media"
@@ -24,6 +23,31 @@ resource "aws_route53_record" "media_ipv6" {
   alias {
     name                   = aws_cloudfront_distribution.media.domain_name
     zone_id                = aws_cloudfront_distribution.media.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+# Load Balancer
+resource "aws_route53_record" "lb_ipv4" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "*"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "lb_ipv6" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "*"
+  type    = "AAAA"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
     evaluate_target_health = true
   }
 }
